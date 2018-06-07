b0fed03ed198b32ad43f3dd5147e84ffad8f0589
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-06-07 18:18:01 +0530

    Classes & TypeScript!

---
+++ ts/script2.ts
@@ -0,0 +1,43 @@
+class Person {
+  public name: string;
+  public username: string
+  private type: string;
+  protected age: number;
+
+  constructor(name: string, username: string) {
+    this.name = name;
+    this.username = username;
+  };
+
+  public printAge(age: number = 0) {
+    this.age = age;
+    this.setType('Geek');
+    return this.age;
+  };
+
+  private setType(type: string) {
+    this.type = type;
+    console.log(this.type);
+  };
+};
+
+class Harry extends Person {  
+  constructor(username: string, age: number) {
+    super('Harman Manchanda', username);
+    this.age = age;
+  };
+};
+
+const userPerson = new Person('Harry Manchanda', 'IamManchanda');
+console.log( userPerson );
+console.log( userPerson.name );
+console.log( userPerson.username );
+console.log( userPerson.printAge(24) );
+
+console.log('------------------------------');
+
+const userHarry = new Harry('IamManchanda', 25);
+console.log( userHarry );
+console.log( userHarry.name );
+console.log( userHarry.username );
+console.log( userHarry.printAge(26) );
