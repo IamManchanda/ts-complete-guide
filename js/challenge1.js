ded7ca0662fe3d6c8275ef43f8816ff1a263d3d1
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-06-07 12:45:36 +0530

    Using types for better code!
    Better code and better VS Code :D

---
+++ js/challenge1.js
@@ -0,0 +1,16 @@
+"use strict";
+var bankAccount;
+bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    },
+};
+var myself;
+myself = {
+    name: "Harry Manchanda",
+    bankAccount: bankAccount,
+    hobbies: ["Cricket", "Coding"],
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
