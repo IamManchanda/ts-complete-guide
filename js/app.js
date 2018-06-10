51e3496bc756eb926bcf76cb08e6df9c348ea1d2
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-06-10 17:20:04 +0530

    Generics!

--- js/app.js
+++ js/app.js
@@ -19,4 +19,33 @@ console.log(betterEcho({
     name: 'Harry',
     age: 27,
 }));
+// Built-in Generics!
+var testResults = [1.94, 2.33];
+testResults.push(-2.99);
+console.log(testResults);
+// Arrays
+var printAll = function (args) {
+    args.forEach(function (currentItem) { return console.log(currentItem); });
+};
+printAll(['Apple', 'Banana']);
+printAll([10, 20]);
+printAll(['Apple', '20']);
+// Generic Types
+var echo2 = echo;
+console.log(echo2('Something'));
+// Generic Class
+var SimpleMath = /** @class */ (function () {
+    function SimpleMath() {
+    }
+    SimpleMath.prototype.calculate = function () {
+        return +this.baseValue * +this.multiplyValue;
+    };
+    ;
+    return SimpleMath;
+}());
+;
+var sm = new SimpleMath();
+sm.baseValue = '10';
+sm.multiplyValue = 20;
+console.log(sm.calculate());
 //# sourceMappingURL=app.js.map
--- ts/app.ts
+++ ts/app.ts
@@ -21,3 +21,35 @@ console.log( betterEcho({
   name: 'Harry',
   age: 27,
 }) );
+
+// Built-in Generics!
+const testResults: number[] = [1.94, 2.33];
+testResults.push(-2.99);
+console.log(testResults);
+
+// Arrays
+const printAll = <T>(args: T[]) => {
+  args.forEach(currentItem => console.log(currentItem));
+};
+printAll<string>(['Apple', 'Banana']);
+printAll<number>([10, 20]);
+printAll<string | number>(['Apple', '20']);
+
+// Generic Types
+const echo2: <T>(data: T) => T = echo;
+console.log( echo2<string>('Something') );
+
+// Generic Class
+class SimpleMath<T extends number | string, U extends number | string> {
+  public baseValue: T;
+  public multiplyValue: U;
+
+  public calculate(): number {
+    return +this.baseValue * +this.multiplyValue;
+  };
+};
+
+const sm = new SimpleMath<string, number>();
+sm.baseValue = '10';
+sm.multiplyValue = 20;
+console.log( sm.calculate() );
